63d2b995cfec2892960a7410fbe484363f47a031
pSQuar3 <pratyaypande@gmail.com>
2021-01-30 21:36:54 +0530

    Updates

--- js/index.js
+++ js/index.js
@@ -8,7 +8,8 @@ import Introduction from "./Introduction.js";
 const e = React.createElement;
 const rend = ReactDOM.render;
 const ge = document.getElementById;
-var iT = "College Graduate attentive to Data Analytics. Passionate about Econometrics, Quantitative Modelling and Financial Analysis. Skilled in Econometrics, R, Python and Excel, I am seeking to leverage my skills in data analytics and enhance my performance and expand my skill set.";
+var iT="B. Sc, Economics,Presidency University, Kolkata";
+var aboutme = "College Graduate attentive to Data Analytics. Passionate about Econometrics, Quantitative Modelling and Financial Analysis. Skilled in Econometrics, R, Python and Excel, I am seeking to leverage my skills in data analytics and enhance my performance and expand my skill set.";
 
 
 ReactDOM.render(e(
