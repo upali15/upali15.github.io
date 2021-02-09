ff9c8500c65db3a117072036b40655fbd562ce5e
pSQuar3 <pratyaypande@gmail.com>
2021-02-09 12:12:41 +0530

    Link redirect Removed pevious script to embed pdf files

--- js/Embed.js
+++ js/Embed.js
@@ -3,7 +3,7 @@
 const e = React.createElement;
 const re = ReactDOM.render;
 
-export default class Embed extends React.Component
+/*export default class Embed extends React.Component
 {
     constructor(props){super(props);}
     render()
@@ -11,5 +11,14 @@ export default class Embed extends React.Component
         return e(
             'embed',{id:this.props.id,src:this.props.path,width:this.props.width,height:this.props.height}
         );
+    }
+}
+*/
+export default class Clickable extends React.Component
+{
+    constructor(props){super(props);}
+    render()
+    {
+        return e('a',{href:this.props.link,target:this.props.target,className:this.props.clss},this.props.text);
     }
 }
